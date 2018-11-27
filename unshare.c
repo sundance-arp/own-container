ffd28005250b7a89b7c146d58e0f3d3ee4df45c8
sundance-arp <black.meimei.0209@gmail.com>
2018-11-28 02:18:27 +0900

    Update MS_PRIVATh flag position

--- unshare.c
+++ unshare.c
@@ -1,81 +1,91 @@
 #define _GNU_SOURCE
+#include <sys/mount.h>
 #include <sched.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/mount.h>
 #include <sys/wait.h>
+
 
 int main(int argc, char *argv[])
 {
   int flags = 0;
-	int rc=0; 
+  int rc=0;
+
+  rc = mount("none", "/", NULL, MS_PRIVATE | MS_REC, NULL);
+  if(rc < 0){
+    printf("/ mount Error: %d\n", rc);
+    return(rc);
+  }
 
   flags |= CLONE_NEWPID;
   flags |= CLONE_NEWNS;
+  //flags |= CLONE_NEWNET;
   //flags |= CLONE_NEWUTS;
   //flags |= CLONE_NEWIPC;
-  //flags |= CLONE_NEWNET;
-	rc = unshare(flags);
+  rc = unshare(flags);
   if(rc < 0){
     printf("unshare Error: %d\n", rc);
   }
 
-	int pid = fork();
-	switch(pid){
+  int pid = fork();
+  switch(pid){
     // error
-		case -1:
-		  printf("fork Error: %d\n", pid);
-			break;
-    // child
-		case 0:
-			break;
-    // parent
-		default:
-			{
-				int status;
-				waitpid(pid,&status,WUNTRACED);
-				if(WIFEXITED(status)){
-					printf("exit status = %d\n",WEXITSTATUS(status));
+    case -1:
+      printf("fork Error: %d\n", pid);
+      break;
+      // child
+    case 0:
+      break;
+      // parent
+    default:
+      {
+        int status;
+        waitpid(pid,&status,WUNTRACED);
+        if(WIFEXITED(status)){
+          printf("exit status = %d\n",WEXITSTATUS(status));
           return WEXITSTATUS(status);
-				}else{
-					printf("exit abnomally\n");
+        }else{
+          printf("exit abnomally\n");
           return -1;
-				}
-			}
-			break;
-	}
+        }
+      }
+      break;
+  }
 
-
-	rc = chdir("./alpine-test");
+  rc = chdir("./alpine-test");
   if(rc < 0){
     printf("chdir Error: %d\n", rc);
   }
-	rc = chroot("./");
-	if(rc < 0){
-		printf("chroot Error: %d\n", rc);
-		return(-1);
-	}
+  rc = chroot("./");
+  if(rc < 0){
+    printf("chroot Error: %d\n", rc);
+    return(-1);
+  }
 
-  rc = mount("proc", "./proc", "proc", MS_NOSUID|MS_NOEXEC|MS_NODEV|MS_MGC_VAL, NULL);
-	if(rc < 0){
-		printf("proc mount Error: %d\n", rc);
-		return(-1);
-	}
-	//rc = mount("devpts","/dev/pts", "devpts", MS_BIND, 0);
-	//if(rc < 0){
-	//    printf("devpts mount Error: %d\n", rc);
-	//    return(-1);
-	//}
-	//mount -t devpts devpts /dev/pts
-	//mount("devpts", "/dev/pts", "devpts",0,0);
+  rc = mount("proc", "./proc", "proc", MS_NOSUID|MS_NOEXEC|MS_NODEV, NULL);
+  if(rc < 0){
+    printf("proc mount Error: %d\n", rc);
+    return(-1);
+  }
+  rc = mount("dev","./dev", "dev", MS_BIND | MS_REC, 0);
+  if(rc < 0){
+    printf("dev mount Error: %d\n", rc);
+    return(-1);
+  }
+  //rc = mount("devpts","/dev/pts", "devpts", MS_BIND, 0);
+  //if(rc < 0){
+  //    printf("devpts mount Error: %d\n", rc);
+  //    return(-1);
+  //}
+  //mount -t devpts devpts /dev/pts
+  //mount("devpts", "/dev/pts", "devpts",0,0);
 
-	rc = execl("/bin/sh","/bin/sh", NULL);
-	if(rc < 0){
-		printf("exec Error: %d\n", rc);
-		return(-1);
-	}
+  rc = execl("/bin/sh","/bin/sh", NULL);
+  if(rc < 0){
+    printf("exec Error: %d\n", rc);
+    return(-1);
+  }
 
 }
-
 
