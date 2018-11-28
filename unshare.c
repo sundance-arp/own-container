19fbd0e1612a1e58cbeed006533066792f55c17e
sundance-arp <black.meimei.0209@gmail.com>
2018-11-29 02:34:32 +0900

    Add cgroup for pid restriction

--- unshare.c
+++ unshare.c
@@ -5,7 +5,51 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
+#include <limits.h>
 
+
+
+int mkdir_p(const char *path,mode_t mode)
+{
+  /* Adapted from http://stackoverflow.com/a/2336245/119527 */
+  const size_t len = strlen(path);
+  char _path[PATH_MAX];
+  char *p;
+
+  errno = 0;
+
+  /* Copy string so its mutable */
+  if (len > sizeof(_path)-1) {
+    errno = ENAMETOOLONG;
+    return -1;
+  }
+  strcpy(_path, path);
+
+  /* Iterate the string */
+  for (p = _path + 1; *p; p++) {
+    if (*p == '/') {
+      /* Temporarily truncate */
+      *p = '\0';
+
+      if (mkdir(_path, mode) != 0) {
+        if (errno != EEXIST)
+          return -1;
+      }
+
+      *p = '/';
+    }
+  }
+
+  if (mkdir(_path, mode) != 0) {
+    if (errno != EEXIST)
+      return -1;
+  }
+
+  return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -42,6 +86,7 @@ int main(int argc, char *argv[])
       {
         int status;
         waitpid(pid,&status,WUNTRACED);
+        rmdir("/sys/fs/cgroup/pids/alpine-test");
         if(WIFEXITED(status)){
           printf("exit status = %d\n",WEXITSTATUS(status));
           return WEXITSTATUS(status);
@@ -52,6 +97,49 @@ int main(int argc, char *argv[])
       }
       break;
   }
+
+  // cgdirの存在確認
+  struct stat st;
+  // cgroupのディレクトリがあるかどうか
+  stat("/sys/fs/cgroup", &st);
+  if((st.st_mode  & S_IFMT) == S_IFDIR){
+    // あればなにもしない
+  }else {
+    // なければ作る
+    rc = mkdir_p("/sys/fs/cgroup",0755);
+    if(rc < 0){
+      printf("cgroup dir create Error: %d\n", rc);
+      return(rc);
+    }
+    // なければマウント
+    rc = mount("cgroup","/sys/fs/cgroup", "cgroup", 0, 0);
+    if(rc < 0){
+      printf("cgroup mount Error: %d\n", rc);
+      return(rc);
+    }
+  }
+
+  // コンテナ用のcgroupディレクトリ作成
+  mkdir("/sys/fs/cgroup/pids/alpine-test",0755);
+  // cgroupへpidの書き込み
+  FILE *cgroup_file;
+  cgroup_file = fopen("/sys/fs/cgroup/pids/alpine-test/tasks","a+");
+  if (cgroup_file == NULL) {
+    printf("cgroup task file open Error\n");
+    exit(1);
+  }
+  // cgroupへpidの上限の書き込み
+  int container_process_pid = getpid();
+  fprintf(cgroup_file,"%d\n",container_process_pid);
+  fclose(cgroup_file);
+  cgroup_file = fopen("/sys/fs/cgroup/pids/alpine-test/pids.max","a+");
+  if (cgroup_file == NULL) {
+    printf("cannot open\n");
+    exit(1);
+  }
+  fprintf(cgroup_file,"%d\n",100);
+  fclose(cgroup_file);
+
 
   rc = chdir("./alpine-test");
   if(rc < 0){
@@ -73,6 +161,7 @@ int main(int argc, char *argv[])
     printf("dev mount Error: %d\n", rc);
     return(-1);
   }
+
   //rc = mount("devpts","/dev/pts", "devpts", MS_BIND, 0);
   //if(rc < 0){
   //    printf("devpts mount Error: %d\n", rc);
