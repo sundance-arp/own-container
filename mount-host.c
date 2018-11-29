891cae186b1342a2d6d336a86301563f5a1da7c3
sundance-arp <black.meimei.0209@gmail.com>
2018-11-30 02:13:36 +0900

    Add cgroup fs mount function

--- mount-host.c
+++ mount-host.c
@@ -1,7 +1,34 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mount.h>
+#include <sys/stat.h>
+
+#include "utils.h"
 
 int mount_host_root(){
   return mount("none", "/", NULL, MS_PRIVATE | MS_REC, NULL);
+}
+
+int mount_cgroup_fs(){
+  // cgdirの存在確認
+  struct stat st;
+  // cgroupのディレクトリがあるかどうか
+  stat("/sys/fs/cgroup", &st);
+  if((st.st_mode  & S_IFMT) == S_IFDIR){
+    // あればなにもしない
+  }else {
+    // なければ作る
+    int rc = mkdir_p("/sys/fs/cgroup",0755);
+    if(rc < 0){
+      printf("cgroup dir create Error: %d\n", rc);
+      return(rc);
+    }
+    // なければマウント
+    rc = mount("cgroup","/sys/fs/cgroup", "cgroup", 0, 0);
+    if(rc < 0){
+      printf("cgroup mount Error: %d\n", rc);
+      return(rc);
+    }
+  }
 }
--- mount-host.h
+++ mount-host.h
@@ -1,1 +1,2 @@
 int mount_host_root();
+int mount_cgroup_fs();
