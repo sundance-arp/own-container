88580a3220b489c18f8ee68c78746d3697842141
sundance-arp <black.meimei.0209@gmail.com>
2018-11-30 02:59:24 +0900

    Add build file manage-cgroups.c

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ if [ "${1}" = "--debug" ]; then
   flag="${flag} -g"
 fi
 
-files="main.c mount-host.c unshare-namespace.c utils.c"
+files="main.c mount-host.c unshare-namespace.c utils.c manage-cgroups.c"
 
 gcc ${flag} ${files} -static -o bin/container
