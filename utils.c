a900fbf0d01636622b05bbe405e9d2105d522d92
sundance-arp <black.meimei.0209@gmail.com>
2018-11-30 02:13:18 +0900

    Add utils module

---
+++ utils.c
@@ -0,0 +1,46 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <limits.h>
+#include <errno.h>
+
+int mkdir_p(const char *path,mode_t mode)
+{
+  /* Adapted from http://stackoverflow.com/a/2336245/119527 */
+  const size_t len = strlen(path);
+  char _path[PATH_MAX];
+  char *p;
+
+  errno = 0;
+
+  /* Copy string so its mutable */
+  if (len > sizeof(_path)-1) {
+    errno = ENAMETOOLONG;
+    return -1;
+  }
+  strcpy(_path, path);
+
+  /* Iterate the string */
+  for (p = _path + 1; *p; p++) {
+    if (*p == '/') {
+      /* Temporarily truncate */
+      *p = '\0';
+
+      if (mkdir(_path, mode) != 0) {
+        if (errno != EEXIST)
+          return -1;
+      }
+
+      *p = '/';
+    }
+  }
+
+  if (mkdir(_path, mode) != 0) {
+    if (errno != EEXIST)
+      return -1;
+  }
+
+  return 0;
+}
---
+++ utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdlib.h>
+int mkdir_p(const char *path,mode_t mode);
+
+#endif
+
