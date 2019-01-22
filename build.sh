5e7c776988f6dac9c2e061d6c58e927773514fb6
sundance-arp <black.meimei.0209@gmail.com>
2019-01-23 03:20:01 +0900

    Add setcap

--- build.sh
+++ build.sh
@@ -10,4 +10,5 @@ fi
 files="main.cpp mount.c namespace.c utils.c cgroups.c ptrace.c"
 
 $(cd ${src_dir}; g++ ${flag} ${files} -static -o ${project_dir}/bin/container)
+sudo setcap CAP_DAC_OVERRIDE,CAP_SYS_CHROOT,CAP_SYS_ADMIN=eip ${project_dir}/bin/container
 
