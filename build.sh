ca2dbd8616019880d4c1263ec5e9b34ed2dbaf68
sundance-arp <black.meimei.0209@gmail.com>
2019-01-25 17:12:14 +0900

    Update filename. main.cpp to main.cc

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ if [ "${1}" = "--debug" ]; then
   flag="${flag} -g"
 fi
 
-files="main.cpp mount.c namespace.c utils.c cgroups.c ptrace.c"
+files="main.cc mount.c namespace.c utils.c cgroups.c ptrace.c"
 
 $(cd ${src_dir}; g++ ${flag} ${files} -static -o ${project_dir}/bin/container)
 sudo setcap CAP_DAC_OVERRIDE,CAP_SYS_CHROOT,CAP_SYS_ADMIN=eip ${project_dir}/bin/container
