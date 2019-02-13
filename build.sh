ce482b34934ced509f54c3470efedb3ac2328188
sundance-arp <black.meimei.0209@gmail.com>
2019-02-14 01:40:57 +0900

    Add commandargument.cc to build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ if [ "${1}" = "--debug" ]; then
   flag="${flag} -g"
 fi
 
-files="main.cc mount.c namespace.cc utils.c cgroups.c ptrace.c"
+files="main.cc mount.c namespace.cc utils.c cgroups.c ptrace.c commandargument.cc"
 
 $(cd ${src_dir}; g++ ${flag} ${files} -static -o ${project_dir}/bin/container)
 sudo setcap CAP_DAC_OVERRIDE,CAP_SYS_CHROOT,CAP_SYS_ADMIN=eip ${project_dir}/bin/container
