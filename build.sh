3ad025f8cf498ef62f9747e86a9572b688966027
sundance-arp <black.meimei.0209@gmail.com>
2019-03-05 00:47:13 +0900

    Fix build error

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 project_dir=$(cd $(dirname $0); pwd)
 src_dir="${project_dir}/src"
 
-flag="-std=c++17 -Wall"
+flag="-std=c++17 -lstdc++fs -Wall"
 if [ "${1}" = "--debug" ]; then
   flag="${flag} -g"
 fi
 
 files="main.cc mount.cc namespace.cc utils.c cgroups.c ptrace.c commandargument.cc"
 
-$(cd ${src_dir}; g++ ${flag} ${files} -static -o ${project_dir}/bin/container)
+$(cd ${src_dir}; g++ ${files} ${flag}  -static -o ${project_dir}/bin/container)
 sudo setcap CAP_DAC_OVERRIDE,CAP_SYS_CHROOT,CAP_SYS_ADMIN=eip ${project_dir}/bin/container
 
