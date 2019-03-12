ffba07b2bc167e88b3d79b1e5b0b128c65cb4874
sundance-arp <black.meimei.0209@gmail.com>
2019-03-12 22:01:33 +0900

    Implement mount host function

--- src/mount.cc
+++ src/mount.cc
@@ -9,6 +9,7 @@
 #include <string.h>
 
 #include <vector>
+#include <fstream>
 #include <string>
 #include <filesystem>
 
@@ -83,10 +84,79 @@ std::vector<std::string> string_split(std::string base, std::string delimiter){
   return result;                                                                
 }                                                                               
 
-// TODO: Implimentation for check
-int mount_host_to_container(string host_path, string container_path, std::filesystem::path rootfs_path){
-  printf("host_path: %s, container_path: %s, rootfs_path: %s , container path: %s\n",host_path.c_str(), container_path.c_str(), rootfs_path.c_str(),(rootfs_path.string() + "/" +container_path).c_str());
-  int rc = mount(host_path.c_str(), (rootfs_path.string() + "/" +container_path).c_str(), NULL, MS_BIND|MS_REC, NULL);
+int mount_host_to_container(std::filesystem::path host_path, std::filesystem::path container_path, std::filesystem::path rootfs_path){
+  // abspath check
+  if(!host_path.is_absolute()){
+    printf("host path is not absolute path Error: %d\n", -1);
+    printf("host path is must absolute path.\n");
+    return -1;
+  }
+  if(!container_path.is_absolute()){
+    printf("container path is not absolute path Error: %d\n", -1);
+    printf("container path is must absolute path.\n");
+    return -1;
+  }
+
+  if (!std::filesystem::exists(rootfs_path)){
+    printf("rootfs path invalid Error%d\n",-1);
+    printf("rootfs is not exist.\n");
+    return -1;
+  }
+
+  if(!std::filesystem::is_directory(rootfs_path)){
+    printf("rootfs path invalid Error%d\n",-1);
+    printf("rootfs is not directory.\n");
+    return -1;
+  }
+
+  // rootfs + container_path
+  std::filesystem::path rootfs_and_container_path;
+  if(rootfs_path.string().back() == '/'){
+    rootfs_and_container_path = std::filesystem::path(rootfs_path.string().substr(0,rootfs_path.string().size()-1) + container_path.string());
+  }else {
+    rootfs_and_container_path = std::filesystem::path(rootfs_path.string() + container_path.string());
+  }
+
+  // exist check
+  if(!std::filesystem::exists(host_path)){
+    printf("host path not found Error%d\n",-1);
+    return -1;
+  }
+
+  // file type check
+  if(std::filesystem::exists(rootfs_and_container_path)){
+    // if container path is exists, then check filetype
+    if(std::filesystem::is_directory(host_path) ^ std::filesystem::is_directory(rootfs_and_container_path)){
+      std::string host_path_file_type;
+      std::string rootfs_and_container_path_file_type;
+
+      if(std::filesystem::is_directory(host_path)){
+        host_path_file_type = "directory";
+      }else {
+        host_path_file_type = "not directory";
+      }
+
+      if(std::filesystem::is_directory(rootfs_and_container_path)){
+        rootfs_and_container_path_file_type = "directory";
+      }else {
+        rootfs_and_container_path_file_type = "not directory";
+      }
+
+      printf("invalid mount point Error%d\n",-1);
+      printf("host path is %s, but container path is %s.\n",host_path_file_type.c_str(), rootfs_and_container_path_file_type.c_str());
+      return -1;
+    }
+  }else {
+    // Create a mount point according to file type
+    if(std::filesystem::is_directory(host_path)){
+      mkdir_p(rootfs_and_container_path.c_str(), 0744);
+    }else {
+      std::ofstream(rootfs_and_container_path.string());
+      std::filesystem::permissions(rootfs_and_container_path, std::filesystem::perms::owner_all | std::filesystem::perms::group_read | std::filesystem::perms::others_read);
+    }
+  }
+
+  int rc = mount(host_path.string().c_str(), (rootfs_path.string() + "/" + container_path.string()).c_str(), NULL, MS_BIND|MS_REC, NULL);
   if(rc < 0){
     printf("host path mount to container Error: %d\n", rc);
     return(-1);
@@ -104,7 +174,7 @@ int mount_host_path(vector<string> mount_paths, std::filesystem::path rootfs_pat
       return -1;
     }
 
-    int rc = mount_host_to_container(from_to_paths[0], from_to_paths[1], rootfs_path);
+    int rc = mount_host_to_container(std::filesystem::path(from_to_paths[0]),std::filesystem::path(from_to_paths[1]),std::filesystem::path(rootfs_path));
     if(rc < 0){
       return rc;
     }
